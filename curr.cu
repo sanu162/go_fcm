d4b7bbbd284cd5a5b204e1e106d00a3068e6207d
suryakant <suryakanta@appscrip.co>
2025-11-14 11:06:09 +0530

    Add initial curl command for sending notifications

---
+++ curr.cu
@@ -0,0 +1,7 @@
+curl --location 'http://localhost:8080/send' \
+--header 'Content-Type: application/json' \
+--data '{
+    "token":"cf6U_xo56lFlSHg9Oc7S3h:APA91bFoTO23xmRuR-4gDLT7tKfoHM8qmhcgylu3Xw1-xrp_OkXeyJZGoEPxLJXFzGfhDSd13O6HsNOR5jWBol7NaqcNCcYVsrHWRUETrJg7CYsrq8ow08Y",
+    "title":"Hello Flexcrew",
+    "body":"Hello World"
+}'
